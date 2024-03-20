82a731ec3d8240799083138715b36103f576fa54
neoki <ot07neoki@gmail.com>
2024-03-20 22:42:24 +0900

    chore: update config files (#10)

--- packages/wc/stylelint.config.mjs
+++
@@ -1,3 +0,0 @@
-import baseConfig from '@color-blindness-devtool/base-configs/stylelint.lit.config.mjs'
-
-export default baseConfig
---
+++ packages/wc/vitest.config.js
@@ -0,0 +1,3 @@
+import baseConfig from '@color-blindness-devtool/base-configs/vitest.base.js'
+
+export default baseConfig
